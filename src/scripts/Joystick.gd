4d7c13a9d443534f304fff41baa72771b29bd94d
Eduardo Barreto <eduardopontobarreto@gmail.com>
2023-06-09 01:43:12 -0300

    feat: :sparkles: inner circle in virtual joystick

--- src/scripts/Joystick.gd
+++ src/scripts/Joystick.gd
@@ -12,15 +12,17 @@ func calculate_move_vector(event_position):
 	joystick_vector.x = clamp(joystick_vector.x, -1, 1)
 	joystick_vector.y = clamp(joystick_vector.y, -1, 1)
 	return joystick_vector
-
+	
 func _input(event):
 	if (event is InputEventScreenTouch or event is InputEventScreenDrag):
 		if is_pressed():
 			move_vector = calculate_move_vector(event.position)
 			joystick_active = true
+			$Pointer.position = (move_vector)*90 + Vector2(90, 90)
 
 	if event is InputEventScreenTouch and not event.pressed:
 		joystick_active = false
+		$Pointer.position = Vector2(90, 90)
 
 func _process(_delta):
 	emit_signal("joystick_input", move_vector)
