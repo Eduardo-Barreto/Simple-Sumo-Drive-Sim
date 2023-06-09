5eadc861c45e96bd741fb855d2c9cdebe4087b03
Eduardo Barreto <eduardopontobarreto@gmail.com>
2023-06-09 01:27:34 -0300

    fix: :adhesive_bandage: call deferred to emit signal and free

--- src/scripts/Collectible.gd
+++ src/scripts/Collectible.gd
@@ -4,6 +4,6 @@ signal hit
 
 func _on_body_entered(body:Node):
 	if body is KinematicBody2D:
-		emit_signal("hit")
-		queue_free()
+		call_deferred("emit_signal", "hit")
+		call_deferred("queue_free")
 		pass
